ee7ef27a8ae72a167e1bdc01c6306eee4f53788e
nisha0712 <nishauforever2@gmail.com>
2019-03-24 10:34:58 +0530

    Recommendation v2

--- recommendation/course.py
+++ recommendation/course.py
@@ -7,5 +7,12 @@ rating=course.groupby(['Course'])['Rating'].mean()
 
 
 course=pd.merge(course1,rating,on='Course')
+
+course2=pd.read_csv('data2.csv')
+course=pd.merge(course,course2,on='Course')
+
+out=course.to_json(orient='table')
+with open('json.txt','w') as f:
+	f.write(out)
 course.to_csv("result.csv")
 print(course)
